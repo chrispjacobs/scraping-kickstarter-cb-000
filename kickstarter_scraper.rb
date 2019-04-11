e5c265499e9f9ced4a28dbcc89969d4b05ebdf0f
Christopher Jacobs <chrisj.3222@gmail.com>
2019-04-11 01:37:23 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,6 +7,7 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   binding.pry
   # kickstarter.css("li.project.grid_4")
+  # title: project.css("h2.bbcard_name strong a")
 end
 
 create_project_hash
